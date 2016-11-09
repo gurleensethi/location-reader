005c36be33479660ec4fa8b4153c8cd9be6ce254
gurleensethi <sarusethi@rocketmail.com>
2016-11-10 02:38:23 +0530

    Make the basic interface

---
+++ app/src/main/java/app/com/thetechnocafe/locationreader/MainLocationReader/MVPContracts.java
@@ -0,0 +1,17 @@
+package app.com.thetechnocafe.locationreader.MainLocationReader;
+
+/**
+ * Created by gurleensethi on 10/11/16.
+ */
+
+public class MVPContracts {
+    //View Contract
+    public interface IView {
+
+    }
+
+    //Presenter Contract
+    public interface IPresenter {
+
+    }
+}
---
+++ app/src/main/java/app/com/thetechnocafe/locationreader/MainLocationReader/MainActivity.java
@@ -0,0 +1,52 @@
+package app.com.thetechnocafe.locationreader.MainLocationReader;
+
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+
+import app.com.thetechnocafe.locationreader.R;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    //Configure the Google Api Client
+
+}
